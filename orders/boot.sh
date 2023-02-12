23f0023372c29c845479651f0e0d266080459582
mikhailBrann <alunos13@yandex.com>
2023-02-12 22:52:04 +0300

    main разобрался с проблемой вывода статики для админки

--- orders/boot.sh
+++ orders/boot.sh
@@ -4,5 +4,5 @@ python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
 python manage.py createsuperuser --email=$DJANGO_SUPERUSER_EMAIL --noinput
-
-gunicorn orders.wsgi --bind 0.0.0.0:8000
+python manage.py collectstatic --no-input
+python manage.py runserver 0.0.0.0:8000
--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from __future__ import absolute_import
 import os
+from pathlib import Path
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -121,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/orders/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
